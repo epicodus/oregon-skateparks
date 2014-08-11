91d8a94fbef49d521d0e6d2822932661f54aeb9a
Nick Bernard <nick@nrbernard.com>
2014-08-11 00:13:20 -0700

    Set up files to start project

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'pg'
+
+require 'city'
+require 'feature'
+require 'park'
+
+DB = PG.connect({:dbname => 'skateparks_test'})
+
+RSpec.configure do |config|
+  config.before(:each) do
+    DB.exec("DELETE FROM cities *;")
+    DB.exec("DELETE FROM features *;")
+    DB.exec("DELETE FROM parks *;")
+    DB.exec("DELETE FROM parks_features *;")
+  end
+end
+
